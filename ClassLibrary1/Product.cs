c0327e360df69e0872aebd93b0b2ceb5bbde9c0a
Андрей Тарашкевич <andrew.tarashkevich9@gmail.com>
2021-12-02 00:02:11 +0300

    Add project files.

---
+++ ClassLibrary1/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public struct Product
+    {        
+        public string Name { get; set; }
+        public string Shop { get; set; }
+
+        public static Product[] SortbyShop(Product[] products)
+        {
+            Product[] products2 = new Product[products.Length];
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < products.Length - 1; i++)
+                {
+                    if (products[i].Shop.CompareTo(products[i + 1].Shop) > 0)
+                    {
+                        products2[i] = products[i];
+                        products[i] = products[i + 1];
+                        products[i + 1] = products2[i];
+                        flag = true;
+                    }
+                }
+            }
+            return products;
+        }
+        public static List<Product> ProductsInShop(Product[] products,string shopName)
+        {
+            List<Product> products2 = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.Shop == shopName)
+                {
+                    products2.Add(product);
+                }                
+            }
+            if (products2.Count == 0)
+            {
+                throw new MyException($"магазина с названием {shopName} не существует");
+            }
+
+            return products2;
+        }
+    }
+}
---
+++ Exercise13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using ClassLibrary1;
+using System.Collections.Generic;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {             
+            Product p1 = new Product { Name="Bread", Shop = "G" };
+            Product p2 = new Product { Name = "Paper", Shop = "C" };
+            Product p3 = new Product { Name = "Orange", Shop = "A" };
+            Product p4 = new Product { Name = "Apple", Shop = "C" };
+            Product p5 = new Product { Name = "Coat", Shop = "C" };
+            Product p6 = new Product { Name = "Laptop", Shop = "B" };
+
+            Product[] products = new Product[6] { p1,p2,p3,p4,p5,p6};
+            Console.WriteLine("\nBefore sort:");
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.Name} - {item.Shop}");
+            }            
+           
+            products = Product.SortbyShop(products);
+
+            Console.WriteLine("\nAfter sort:");
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.Name} - {item.Shop}");
+            }
+            Console.WriteLine("\nEnter shop:");
+            string shopName = Console.ReadLine();
+            List<Product> productsInShop = Product.ProductsInShop(products, shopName);
+            foreach (var item in productsInShop)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }        
+    }
+}
